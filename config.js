1f2ce8c12fd7cff91c54e64468f5f8a627fb516b
zillawisj <dechpaksirawich@gmail.com>
2024-04-25 11:54:27 +0700

    add config.js and update package.json

---
+++ config.js
@@ -0,0 +1,3 @@
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,12 @@
 const { dayOfTheWeek } = require('./app');
+const { monthOfTheYear } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
+});
+test('getMonth returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('November');
 });
